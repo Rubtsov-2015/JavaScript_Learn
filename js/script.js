ecb68757669ae62c7e6dcef6ded67ec31fbf80b7
Alexey <Alexey>
2016-08-30 16:32:47 +0300

    Задача с таймером и кнопками

--- js/script.js
+++ js/script.js
@@ -1,114 +1,42 @@
-function rand (min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+var fullTime = document.getElementById('time');
 
+getTime();
 
-function getMatrix(size) {
-  var arr = [];
-  for (var i = 0; i < size; i++) {
-    arr[i] = [];
-    for (var j = 0; j < size; j++) {
-      arr[i].push(rand(0, 100));
-    }
-  }
-  return arr;
-}
-
-var matrix = getMatrix(10);
-console.table(matrix);
-
-var endArr = [],
-	endArr2 = [],
-	endArr3 = [];
-
-var endSize = 10;
-var S = 0;
-
-
-
-//!!!Ломаю голову над тем, как сделать только с 1 циклом 1-ну из задач.!!!//
-
-
-
-//Идем по периметру против часовой стрелки
-for(i = 0; i < endSize; i++){
-	endArr[endArr.length] = matrix[i][S];
-	if(i === endSize - 1){
-		for(i = 1; i < endSize; i++){
-			endArr[endArr.length] = matrix[endSize-1][i];
-		}
-		for(i = (endSize-2) ; i >= 0; i--){
-			endArr[endArr.length] = matrix[i][endSize-1];
-		}
-		for(i = (endSize-2); i > 0; i-- ){
-			endArr[endArr.length] = matrix[S][i];
-		}
-		break;
+function getTime() {
+	var date = new Date();
+	var hours = date.getHours();
+	var min = date.getMinutes();
+	var sec = date.getSeconds();
+	var millsec = date.getMilliseconds();
+	if (hours < 10){
+		hours = '0' + hours;
 	}
-}
-var count = -1;
-console.log('Против часовой стрелки: ' + endArr);
-for(i = 0; i < endSize; i++){
-	endArr[endArr.length] = matrix[i][S];
-
-}
-
-
-
-//Обходим двумерный массив по диагонали по часовой стрелки с левого 
-//верхнего угла.
-for(i = 0; i < endSize; i++){
-	endArr2[endArr2.length] = matrix[S][i];
-	if(i == endSize - 1){
-		for(i = 1 ; i < endSize; i++){
-			endArr2[endArr2.length] = matrix[i][endSize - 1];
-		}
-		for(i-=2; i > 0; i--){
-			endArr2[endArr2.length] = matrix[i][i];
-		}
-		break;
+	if (min < 10){
+		min = '0' + min;
 	}
-}
-console.log('Диагональ по часовой: ' + endArr2);
-
-
-//Последнее задание
-for(i = 0; i < endSize; i++){
-	endArr3[endArr3.length] = matrix[i][endSize-1];
-}
-for(i = endSize - 2; i >= 0; i--){
-	endArr3[endArr3.length] = matrix[endSize-1][i];
-}
-var q = 0;
-for(i = endSize - 1; i > 0; i--){
-	endArr3[endArr3.length] = matrix[q][i];
-	q++;
-}
-console.log('Последнее: ' + endArr3);
-
-
-//Второй вариант
-//Вот, то что я хотел!
-var testArr = [],
-	testArr2 = [],
-	testArr3 = [],
-	testArr4 = [];
-
-var a = 0, c = endSize-1;
-
-for(var i = 0; i < endSize; i++){
-	testArr[testArr.length] = matrix[i][a];
-	testArr2[testArr2.length] = matrix[endSize-1][i];
-	testArr3[testArr3.length] = matrix[c][endSize-1];
-	testArr4[testArr4.length] = matrix[a][c];
-
-	c--;
-
-	if(i == endSize-1){
-		testArr.pop();
-		testArr2.pop();
-		testArr3.pop();
-		testArr4.pop();
+	if (sec < 10){
+		sec = '0' + sec;
 	}
+	var str = hours + ':' + min +':' + sec + ':' + millsec;
+	document.getElementById('time').innerHTML = str;
+	setTimeout('getTime()', 1000);
+};
+
+//Вторая задачка
+
+var countBtn = document.getElementsByTagName('button').length;
+var btn = document.getElementsByTagName('button');
+
+
+for(i = 0; i < countBtn; i++){
+	setClick(i);
 }
-console.log(testArr.concat(testArr2,testArr3,testArr4));
+
+function setClick(i){
+	var a = i;
+	a++;
+	btn[i].classList.add(a);
+	btn[i].addEventListener("click", function(){
+		alert('Кнопка' + a);
+	});
+};
